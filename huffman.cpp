a8bfcaaa7e9a553dcc07c6a2210cf6e4e514defb
Rita <margaritam2706@gmail.com>
2014-11-14 18:57:01 +0300

    build tree

--- huffman.cpp
+++ huffman.cpp
@@ -3,6 +3,7 @@
 #include "huffman.hpp"
 #include <iostream>
 #include <cstring>
+#include <algorithm>
 
 Node::Node()
 {
@@ -47,12 +48,83 @@ void Huffman::run()
 
 }
 
+bool comp (Node a, Node b) {
+    return a.cnt < b.cnt;
+}
+
 void Huffman::compression()
 {
-    
+    read_vector_cnt_by_char();
+    std::sort(vector.begin(), vector.end(), comp);
+    std::cout<< "\n";
+    print_vector_cnt();
+
+    int begin_vector = 0;
+    while(begin_vector < vector.size() - 1) {
+        Node first = vector[begin_vector];
+        Node second = vector[begin_vector + 1];
+        Node *newNode = new Node(first.cnt + second.cnt, first.string + second.string);
+        newNode->left_child = new Node(first);
+        newNode->right_child = new Node(second);
+        vector[begin_vector + 1] = new Node (newNode);
+        begin_vector++;
+
+        sort(vector.begin() + begin_vector, vector.end(), comp);
+    }
+
+
 }
+
+
 
 void Huffman::decompression()
 {
 
+}
+
+Node::Node(int cnt, std::string string)  : cnt(cnt), string(string)
+{
+ // (:
+}
+
+void Huffman::read_vector_cnt_by_char()
+{
+
+
+    //debug in_file /////////////////////////////
+    std::string in_string = "coffecoff";/////////
+    /////////////////////////////////////////////
+
+    for(int i = 0; i < in_string.length(); ++i) {
+        int num_by_char = in_string[i];
+        cnt[num_by_char]++;
+    }
+
+    for(int i = 0; i < MAX_NUM_BY_CHAR; i++) {
+        if(cnt[i] != 0) {
+            char t = (char) i;
+            std::string s (1, t);
+            Node node(cnt[i], s);
+            vector.push_back(node);
+        }
+    }
+
+    print_vector_cnt();
+
+}
+
+void Huffman::print_vector_cnt()
+{
+    for(int i = 0; i < vector.size(); i++) {
+        std::cout << vector[i].string << "!!!" << vector[i].cnt << std::endl;
+    }
+}
+
+
+Node::Node(Node *pNode)
+{
+    cnt = pNode->cnt;
+    left_child = pNode->left_child;
+    right_child = pNode->right_child;
+    string = pNode->string;
 }
--- huffman.hpp
+++ huffman.hpp
@@ -1,9 +1,13 @@
+#include <vector>
+
 #pragma once
 
 const std::string USAGE = "USAGE: ./huffman [-c|-u] [-f file_name] [-o output]";
 
 class Node {
 public:
+    Node(Node *pNode);
+
     std::string string;
     int cnt;
     Node * left_child;
@@ -11,10 +15,14 @@ public:
 
     Node();
 
+    Node(int cnt, std::string string);
+
     ~Node();
 };
 
 class Huffman {
+    static int const MAX_NUM_BY_CHAR = 256;
+
 public:
     bool is_decompression;
     char * in_file;
@@ -31,6 +39,16 @@ public:
 private:
     void compression();
     void decompression();
+
+    int cnt[MAX_NUM_BY_CHAR]{0};
+
+    void read_vector_cnt_by_char();
+
+    std::vector<Node> vector;
+
+    void print_vector_cnt();
+
+
 };
 
 void print_usage();
